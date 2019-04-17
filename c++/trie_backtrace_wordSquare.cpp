02ed5d3269cb0769167dc8dd50af51fe4ffc9f87
Yong <yongli.pdx@gmail.com>
2019-04-16 18:27:48 -0700

    trie

---
+++ c++/trie_backtrace_wordSquare.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+struct trie{
+  char c;
+	vector<shared_ptr<trie>> child;
+  set<int>
+};
+	
+	
+
+
